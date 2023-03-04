d540fd5953d0c15e8017009733160340a91094f9
Reva22 <91474756+Reva22@users.noreply.github.com>
2023-03-05 00:51:33 +0530

    Time: 80 ms (15.49%), Space: 20.4 MB (11.72%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>v;
+        vector<int>a,b;
+        sort(intervals.begin(), intervals.end());
+        
+        int maxi = intervals[0][1];
+        a.push_back(intervals[0][0]);
+        for(int i=0;i<intervals.size()-1;i++){
+            if(intervals[i+1][0] <= maxi){
+                maxi = max(intervals[i+1][1], maxi);
+            }
+            else{
+                b.push_back(maxi);
+                a.push_back(intervals[i+1][0]);
+                cout<<i<<" "<<a[a.size()-2]<<" "<<b[b.size()-1]<<endl;
+                maxi = intervals[i+1][1];
+            }
+        }
+        b.push_back(maxi);
+        for(int i=0;i<a.size();i++){
+            vector<int>p;
+            p.push_back(a[i]);
+            p.push_back(b[i]);
+            v.push_back(p);
+        }
+        
+        return v;
+    }
+};
